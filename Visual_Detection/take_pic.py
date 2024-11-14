3706d339c954bf41b881ceb303cf23a55e34c57a
kofib123 <boateng.kofi123@gmail.com>
2024-11-14 18:02:18 -0500

    took pics

--- Visual_Detection/take_pic.py
+++ Visual_Detection/take_pic.py
@@ -1,4 +1,4 @@
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 from time import sleep
 
 # Initialize camera
@@ -7,13 +7,23 @@ picam2 = Picamera2()
 # Configure and start the camera
 config = picam2.create_still_configuration()
 picam2.configure(config)
+
+
 picam2.start()
+num_pics =5
+intervals = 2
 
-# Capture image
-sleep(2)  # Let the camera adjust to lighting
-image_path = "/home/pi/captured_image.jpg"
-picam2.capture_file(image_path)
+print("starting")
+for i in range(11,16):
+    # Allow time for adjusting and viewing preview 
+
+    # Capture image
+    # Let the camera adjust to lighting
+    image_path = f"/home/ODYSSY/important_code/Visual_Detection/captured_image{i+1}.jpg"
+    picam2.capture_file(image_path)
+    print(f"Image {i+1} saved at {image_path}")
+    sleep(intervals)
 picam2.stop()
+print("Done")
 
-print(f"Image saved at {image_path}")
 
